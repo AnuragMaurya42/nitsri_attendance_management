0465e4a5ff87f3fb8c5b297d8c6438ac7c6451d5
AnuragMaurya42 <am9648676889@gmail.com>
2025-05-28 12:33:35 +0530

    change the UI header and footer23456

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
